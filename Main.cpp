1ed0ae76282082bb4234820cb4fee48f12d21071
aleks709 <aleksbahovskiy@gmail.com>
2026-05-11 12:31:43 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,19 +4,17 @@
 using namespace std;
 
 int main() {
-    // Тут ми вводиимо координати трикутника
     Triangle t;
     cout << "Введіть координати трикутника:" << endl;
     cout << "A (x y): "; cin >> t.A.x >> t.A.y;
     cout << "B (x y): "; cin >> t.B.x >> t.B.y;
     cout << "C (x y): "; cin >> t.C.x >> t.C.y;
 
+    
     if (t.isDegenerate()) {
         cout << "Трикутник вироджений (площа = 0)!" << endl;
-        return 0;
     }
 
-    // А тут довільна кількості точок
     int n;
     cout << "Скільки точок перевірити? "; cin >> n;
 
@@ -24,12 +22,21 @@ int main() {
         Point p;
         cout << "Точка " << i + 1 << " (x y): "; cin >> p.x >> p.y;
 
-        if (t.onBorder(p)) {
-            cout << "Точка лежить на межі трикутника" << endl;
-        } else if (t.contains(p)) {
-            cout << "Точка належить трикутнику" << endl;
+        if (t.isDegenerate()) {
+            // Для виродженого трикутника перевіряємо чи точка лежить на тій самій прямій
+            if (t.onBorder(p)) {
+                cout << "Точка лежить на виродженому трикутнику (прямій)" << endl;
+            } else {
+                cout << "Точка не належить виродженому трикутнику" << endl;
+            }
         } else {
-            cout << "Точка не належить трикутнику" << endl;
+            if (t.onBorder(p)) {
+                cout << "Точка лежить на межі трикутника" << endl;
+            } else if (t.contains(p)) {
+                cout << "Точка належить трикутнику" << endl;
+            } else {
+                cout << "Точка не належить трикутнику" << endl;
+            }
         }
     }
 
